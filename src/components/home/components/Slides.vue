f29551d549af35faecda9ca1a0d90a7f11e64391
CDHZAYN <949135640@qq.com>
2022-09-15 17:32:29 +0800

    feat: home update done

--- src/components/home/components/Slides.vue
+++ src/components/home/components/Slides.vue
@@ -1,19 +1,9 @@
 <template>
   <div style="display: flex;">
     <div style="width: 20vw; height: 25.714vw; position: relative;">
-      <div class="switch" @mouseenter="isBackHover = true" @mouseleave="isBackHover = false"
-           @click="changeDisplay(this.onDisplayIndex-1)" style="right: 10%;top: 70%;">
-          <img src="/arrow.png" alt="back" class="switch-arrow" v-if="!isBackHover"/>
-          <img src="/studio.png" alt="back" class="switch-arrow" v-else/>
-        <div class="switch-scroll">
-          <Transition mode="out-in">
-            <h6 class="switch-title" v-if="!isBackHover" key="q1">BACK</h6>
-            <h6 :class="['switch-title', {'switch-title-on-hover': isBackHover}]" v-else key="q2">
-              {{ items[onDisplayIndex - 1 < 0 ? items.length - 1 : onDisplayIndex - 1].name }}</h6>
-          </Transition>
-        </div>
-      </div>
-      <button style="margin-top: 100px;" @click="isBackHover=!isBackHover">切换</button>
+      <SlidesButton style="right: 20%;bottom: 10%" type="BACK"
+                        :button-name="items[onDisplayIndex - 1 >= 0 ? onDisplayIndex - 1: items.length - 1].name"
+                        @back="changeDisplay"/>
     </div>
     <div class="main">
       <img :src="getUrl(items[onDisplayIndex].pic)" :alt="items[onDisplayIndex].name"
@@ -22,31 +12,23 @@
       <p :class="['main-desc', {'main-img-changing': isMainDescChanging}]">{{ items[onDisplayIndex].desc }}</p>
     </div>
     <div style="width: 20vw; height: 25.714vw; position: relative;">
-      <div class="switch" @mouseenter="isNextHover = true" @mouseleave="isNextHover = false"
-           @click="changeDisplay(this.onDisplayIndex+1)" style="left: 10%;top: 70%;">
-        <div class="switch-scroll">
-            <h6 class="switch-title" v-if="!isNextHover" style="direction: rtl;">NEXT</h6>
-            <h6 :class="['switch-title',{'switch-title-on-hover': isNextHover}]" style="direction: rtl;" v-else>
-              {{ items[onDisplayIndex + 1 > items.length - 1 ? 0 : onDisplayIndex + 1].name }}</h6>
-        </div>
-          <img src="/arrow.png" alt="back" class="switch-arrow" v-if="!isNextHover"
-               style="transform: rotate(180deg) translateY(-3px)">
-          <img src="/studio.png" alt="default" class="switch-arrow" v-else>
-      </div>
+      <SlidesButton style="left: 20%;bottom: 10%" type="NEXT"
+                        :button-name="items[onDisplayIndex + 1 < items.length ? onDisplayIndex + 1: 0].name"
+                        @back="changeDisplay"/>
     </div>
   </div>
 </template>
 
 <script>
 import getAssetsFile from "../../../assets/getAssetsFile.js";
+import SlidesButton from "./SlidesButton.vue";
 
 export default {
   name: "Slides",
+  components: {SlidesButton},
   data() {
     return {
       onDisplayIndex: 2,
-      isBackHover: false,
-      isNextHover: false,
       items: [
         {
           name: 'Definitive Future',
@@ -82,24 +64,23 @@ export default {
     switchTimer: {
       handler(newVal, oldVal) {
         if (newVal % 10 === 0)
-          this.changeDisplay((this.onDisplayIndex + 1) % this.items.length)
+          this.changeDisplay(1)
       }
     }
   },
   methods: {
-    changeDisplay(newIndex) {
-      if (newIndex === this.onDisplayIndex) return
+    changeDisplay(diff) {
       if (this.isMainDescChanging) return
-      // console.log(this.isMainDescChanging)
       this.isMainImgChanging = true
       this.isMainNameChanging = true
       this.isMainDescChanging = true
-      // console.log(this.isMainDescChanging)
 
+      let newIndex = this.onDisplayIndex + diff
       let _this = this
       setTimeout(function () {
+
         if (newIndex < 0)
-          _this.onDisplayIndex = _this.items.length - 1;
+          _this.onDisplayIndex = _this.items.length - 1
         else if (newIndex > _this.items.length - 1)
           _this.onDisplayIndex = 0;
         else
@@ -128,72 +109,6 @@ export default {
 </script>
 
 <style scoped>
-
-@keyframes scroll {
-  0% {
-    margin-left: 0;
-    transform: translateX(0);
-  }
-  100% {
-    margin-left: 100%;
-    transform: translateX(-100%);
-  }
-}
-
-.v-enter-active{
-  transition: 0.5s ease;
-}
-.v-leave-active{
-  transition: 0.5s ease;
-}
-.v-enter-from,.v-leave-to{
-  opacity: 0;
-}
-
-.switch {
-  display: inline-block;
-  width: 150px;
-  height: 30px;
-  position: absolute;
-  margin: 0;
-  padding: 5px;
-
-  box-shadow: -3px 5px 20px rgba(0, 0, 0, .1);
-  border: 3px solid;
-  border-image: linear-gradient(to right top, rgba(255, 255, 255, .0) 50%, rgba(255, 255, 255, .8) 100%) 20 20;
-}
-
-.switch .switch-arrow {
-  width: 25px;
-  transform: translateY(3px);
-}
-
-.switch .switch-scroll {
-  width: 95px;
-  display: inline-block;
-  margin: 0 15px 0 15px;
-  overflow: hidden;
-  transform: translateY(2px);
-}
-
-.switch .switch-scroll .switch-title {
-  min-width: 100%;
-  white-space: nowrap;
-  margin: 0;
-  float: left;
-
-  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, \5fae\8f6f\96c5\9ed1, Arial, sans-serif;
-  font-size: 20px;
-  font-weight: normal;
-
-  position: relative;
-  animation: scroll ease-in-out 3s alternate infinite;
-  animation-play-state: paused;
-}
-
-.switch .switch-scroll .switch-title-on-hover {
-  animation-play-state: running;
-}
 
 .main {
   text-align: center;
---
+++ src/components/home/components/SlidesButton.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="button" @mouseenter="isHover = true" @mouseleave="isHover = false"
+       @click="$emit('back', type === 'BACK' ? -1 : 1)" style="right: 10%;top: 70%;">
+    <div class="button-img-frame" v-if="type==='BACK'">
+      <Transition type="transition" mode="out-in">
+        <img src="/arrow.png" alt="back" class="button-img" v-if="!isHover"/>
+        <img src="/studio.png" alt="back" class="button-img" v-else/>
+      </Transition>
+    </div>
+    <div class="button-word-frame">
+      <Transition type="transition" mode="out-in">
+        <h6 class="button-word" v-if="!isHover" key="q1">{{ type }}</h6>
+        <h6 :class="['button-word', {'button-on-hover': isHover}]" v-else key="q2">
+          {{ buttonName }}</h6>
+      </Transition>
+    </div>
+    <div class="button-img-frame" v-if="type==='NEXT'">
+      <Transition type="transition" mode="out-in">
+        <img src="/arrow.png" alt="back" class="button-img" style="transform: rotate(180deg) translateY(-2px)"
+             v-if="!isHover"/>
+        <img src="/studio.png" alt="back" class="button-img" v-else/>
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SlidesButtonBack",
+  props: {
+    buttonName: String,
+    type: String
+  },
+  data() {
+    return {
+      isHover: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@keyframes scroll {
+  0% {
+    margin-left: 0;
+    transform: translateX(0);
+  }
+  100% {
+    margin-left: 100%;
+    transform: translateX(-100%);
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.button {
+  display: inline-block;
+  width: 150px;
+  height: 30px;
+  position: absolute;
+  margin: 0;
+  padding: 5px;
+
+  box-shadow: -3px 5px 20px rgba(0, 0, 0, .1);
+  border: 3px solid;
+  border-image: linear-gradient(to right top, rgba(255, 255, 255, .0) 50%, rgba(255, 255, 255, .8) 100%) 20 20;
+  background-color: rgba(255, 255, 255, .1);
+}
+
+.button .button-img-frame {
+  display: inline-block;
+}
+
+.button .button-img {
+  width: 25px;
+  transform: translateY(3px);
+}
+
+.button .button-word-frame {
+  width: 95px;
+  display: inline-block;
+  margin: 0 15px 0 15px;
+  overflow: hidden;
+  transform: translateY(2px);
+}
+
+.button .button-word-frame .button-word {
+  min-width: 100%;
+  white-space: nowrap;
+  margin: 0;
+  float: left;
+
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, \5fae\8f6f\96c5\9ed1, Arial, sans-serif;
+  font-size: 20px;
+  font-weight: normal;
+
+  position: relative;
+}
+
+.button .button-word-frame .button-word.button-on-hover {
+  animation: scroll ease-in-out 3s alternate infinite;
+}
+</style>
